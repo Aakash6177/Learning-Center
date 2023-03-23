fbf6483764b6eb43b340c3961cf4102301aa4451
Gurdev Singh <42390570+SinghGurdev@users.noreply.github.com>
2023-03-23 10:36:46 -0700

    Update main.js
    Adding in post endpoint for  Acedemic Success Coaching. Also updating some comments

--- main.js
+++ main.js
@@ -8,15 +8,22 @@ const asc = require("./academicSuccessCoaching/asc.js");
 
 //Endpoint Tutorial Center
 app.get('/tc', function(req,res){
+    //Returns the home screen for Tutorial Center
     tc.homeScreen(req,res);
 });
 //Endpoint Supplemental Instruction
 app.get('/si', function(req,res){
+    //Returns the home screen for Supplemental Instruction
     si.homeScreen(req,res);
 });
 //Endppoint Acedemic Success Coaching
 app.get('/asc', function(req,res){
+    //Returns the home screen for Acedemic Success Coaching
     asc.homeScreen(req,res);
+});
+app.post('/asc/appointment' function(req,res){
+    //fucntion call to handle form submisson goes here
+    res.end();
 });
 
 //Running Server

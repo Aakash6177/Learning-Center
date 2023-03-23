76afd456a93f22f9d7eec542f2315f55d47e58d3
Gurdev Singh <42390570+SinghGurdev@users.noreply.github.com>
2023-03-23 10:15:54 -0700

    Connection to a MongoDB
    Need to add in database details like username and password

---
+++ dataBase.js
@@ -0,0 +1,26 @@
+const {MongoClient} = require('mongodb');
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function connect(){
+    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
